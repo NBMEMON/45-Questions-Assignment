edb9bde57924fb9f8ce1d49ef8dd681287dbc21a
NBMEMON <proinsaan890@gmail.com>
2024-04-05 13:16:55 -0700

    45 Question Assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+// Welcome to My 45 Questions Assignment
+var person_Name = "Ashraf";
+console.log("Hello", person_Name, ", ~Let's learn Typescript today"); //Output : Hello Ashraf,Let's learn Typescript today
+// Second Question Done 
+var Personname = "Naeeb sohail";
+console.log(Personname.toLowerCase());
+console.log(Personname.toUpperCase());
+console.log(Personname.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
+// Third Question Done
+console.log("Nikola Tesla once said ,\"A new idea must not be judged by its immediate results\"");
+// Fourth Question Done 
+var famous_Person = "Nikola Tesla";
+var message = " once said ,\"A new idea must not be judged by its immediate results\"";
+console.log(famous_Person + "" + message);
+//  Fifth Question Done
+var whitespacename = "\t\nNaeeb Sohail\n\t";
+console.log(whitespacename);
+var withoutwhitespacename = (whitespacename.trim());
+console.log(withoutwhitespacename);
---
+++ index.ts
@@ -0,0 +1,20 @@
+ // Welcome to My 45 Questions Assignment
+let person_Name ="Ashraf";
+console.log("Hello",person_Name,", Let's learn Typescript today");  //Output : Hello Ashraf,Let's learn Typescript today
+// Second Question Done 
+let Personname="Naeeb sohail";
+console.log(Personname.toLowerCase());
+console.log(Personname.toUpperCase());
+console.log(Personname.replace(/\b\w/g,(char)=> char.toUpperCase()));
+// Third Question Done
+console.log("Nikola Tesla once said ,\"A new idea must not be judged by its immediate results\"");
+// Fourth Question Done 
+let famous_Person="Nikola Tesla";
+let message= " once said ,\"A new idea must not be judged by its immediate results\"";
+console.log(famous_Person+""+message);
+//  Fifth Question Done
+let whitespacename="\t\nNaeeb Sohail\n\t";
+console.log(whitespacename);
+
+let withoutwhitespacename=(whitespacename.trim());
+console.log(withoutwhitespacename);
