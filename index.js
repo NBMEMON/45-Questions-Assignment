292954f72efce8c6ec9e4c17c7e058c64cb43e98
NBMEMON <proinsaan890@gmail.com>
2024-04-07 14:45:40 -0700

    45 Question Assignment

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 // Welcome to My 45 Questions Assignment
 var person_Name = "Ashraf";
-console.log("Hello", person_Name, ", ~Let's learn Typescript today"); //Output : Hello Ashraf,Let's learn Typescript today
-// Second Question Done 
+console.log("Hello", person_Name, ", Let's learn Typescript today"); //Output : Hello Ashraf,Let's learn Typescript today
+// Second Question Done
 var Personname = "Naeeb sohail";
 console.log(Personname.toLowerCase());
 console.log(Personname.toUpperCase());
 console.log(Personname.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
 // Third Question Done
-console.log("Nikola Tesla once said ,\"A new idea must not be judged by its immediate results\"");
-// Fourth Question Done 
+console.log('Nikola Tesla once said ,"A new idea must not be judged by its immediate results"');
+// Fourth Question Done
 var famous_Person = "Nikola Tesla";
-var message = " once said ,\"A new idea must not be judged by its immediate results\"";
+var message = ' once said ,"A new idea must not be judged by its immediate results"';
 console.log(famous_Person + "" + message);
 //  Fifth Question Done
 var whitespacename = "\t\nNaeeb Sohail\n\t";
 console.log(whitespacename);
-var withoutwhitespacename = (whitespacename.trim());
+var withoutwhitespacename = whitespacename.trim();
 console.log(withoutwhitespacename);
+// Sixth Question Done
+console.log("\t", 2 + 6, "\t");
+console.log("\t", 10 - 3, "\t");
+console.log("\t", 4 * 2, "\t");
+console.log("\t", 64 / 8, "\t");
+// Seventh Question Done
+var fav_Num = "10";
+console.log("My Favourite Number is " + fav_Num);
+// Eight Questions Done
+// Completing the assignment doing the following questions
+// Ninth Question Done
+var friendsName = ["Ashraf", "Nasir", "Abid"];
+friendsName.forEach(function (friendsName) { return console.log(friendsName); });
+// Tenth Question Done
--- index.ts
+++ index.ts
@@ -1,21 +1,56 @@
- // Welcome to My 45 Questions Assignment
-let person_Name ="Ashraf";
-console.log("Hello",person_Name,", Let's learn Typescript today");  //Output : Hello Ashraf,Let's learn Typescript today
-// Second Question Done 
-let Personname="Naeeb sohail";
+// Welcome to My 45 Questions Assignment
+
+let person_Name = "Ashraf";
+console.log("Hello", person_Name, ", Let's learn Typescript today"); //Output : Hello Ashraf,Let's learn Typescript today
+
+// Second Question Done
+
+let Personname = "Naeeb sohail";
 console.log(Personname.toLowerCase());
 console.log(Personname.toUpperCase());
-console.log(Personname.replace(/\b\w/g,(char)=> char.toUpperCase()));
+console.log(Personname.replace(/\b\w/g, (char) => char.toUpperCase()));
+
 // Third Question Done
-console.log("Nikola Tesla once said ,\"A new idea must not be judged by its immediate results\"");
-// Fourth Question Done 
-let famous_Person="Nikola Tesla";
-let message= " once said ,\"A new idea must not be judged by its immediate results\"";
-console.log(famous_Person+""+message);
+
+console.log(
+  'Nikola Tesla once said ,"A new idea must not be judged by its immediate results"'
+);
+
+// Fourth Question Done
+
+let famous_Person = "Nikola Tesla";
+let message =
+  ' once said ,"A new idea must not be judged by its immediate results"';
+console.log(famous_Person + "" + message);
+
 //  Fifth Question Done
-let whitespacename="\t\nNaeeb Sohail\n\t";
+
+let whitespacename = "\t\nNaeeb Sohail\n\t";
 console.log(whitespacename);
 
-let withoutwhitespacename=(whitespacename.trim());
+let withoutwhitespacename = whitespacename.trim();
 console.log(withoutwhitespacename);
+
 // Sixth Question Done
+
+console.log("\t", 2 + 6, "\t");
+console.log("\t", 10 - 3, "\t");
+console.log("\t", 4 * 2, "\t");
+console.log("\t", 64 / 8, "\t");
+
+// Seventh Question Done
+
+let fav_Num = "10";
+console.log("My Favourite Number is " + fav_Num);
+
+// Eight&Ninth Questions Done
+
+// Completing the assignment doing the following questions
+
+// Tenth Question Done
+
+let friendsName = ["Ashraf", "Nasir", "Abid"];       
+friendsName.forEach(friendsName => console.log(friendsName));
+
+// Eleventh Question Done
+
