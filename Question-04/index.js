929949aeacec809943740358cdaa85e1d6f4710b
NBMEMON <proinsaan890@gmail.com>
2024-04-26 12:02:08 -0700

    Question 4

---
+++ Question-04/index.js
@@ -0,0 +1,3 @@
+var famous_Person = "Nikola Tesla";
+var message = ' once said ,"A new idea must not be judged by its immediate results"';
+console.log(famous_Person + "" + message);
---
+++ Question-04/index.ts
@@ -0,0 +1,5 @@
+let famous_Person = "Nikola Tesla";
+let message =
+  ' once said ,"A new idea must not be judged by its immediate results"';
+console.log(famous_Person + "" + message);
+
