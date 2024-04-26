a22f03ffb8cdd4f70d99a07f231cdcd3179d9ae9
NBMEMON <proinsaan890@gmail.com>
2024-04-26 12:04:40 -0700

    Question 5

---
+++ Question-05/index.js
@@ -0,0 +1,4 @@
+var whitespacename = "\t\nNaeeb Sohail\n\t";
+console.log(whitespacename);
+var withoutwhitespacename = whitespacename.trim();
+console.log(withoutwhitespacename);
---
+++ Question-05/index.ts
@@ -0,0 +1,5 @@
+let whitespacename = "\t\nNaeeb Sohail\n\t";
+console.log(whitespacename);
+
+let withoutwhitespacename = whitespacename.trim();
+console.log(withoutwhitespacename);
