a8a8610a3f2ea649aff14d981be31ababcec505c
NBMEMON <proinsaan890@gmail.com>
2024-04-26 11:56:11 -0700

    Question 1

---
+++ Question -01/index.js
@@ -0,0 +1,2 @@
+var person_Name = "Ashraf";
+console.log("Hello", person_Name, ", Let's learn Typescript today"); //Output : Hello Ashraf,Let's learn Typescript today
---
+++ Question -01/index.ts
@@ -0,0 +1,2 @@
+let person_Name = "Ashraf";
+console.log("Hello", person_Name, ", Let's learn Typescript today"); //Output : Hello Ashraf,Let's learn Typescript today
