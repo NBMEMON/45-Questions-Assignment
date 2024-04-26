631bbe9b77bf899daab682feafa37e34475c8009
NBMEMON <proinsaan890@gmail.com>
2024-04-26 12:39:24 -0700

    Question 12

---
+++ Question-12/index.js
@@ -0,0 +1,2 @@
+var friendsName1 = ["Shahruk", "Amir", "Salman"];
+friendsName1.forEach(function (friendsName1) { return console.log("Hey ".concat(friendsName1, " How are you?")); });
---
+++ Question-12/index.ts
@@ -0,0 +1,3 @@
+let friendsName1=["Shahruk","Amir","Salman"];
+friendsName1.forEach(friendsName1 => console.log(`Hey ${friendsName1} How are you?`));
+
