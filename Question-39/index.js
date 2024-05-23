b716000a4f0c3b385eb95a58f42e6c7342530e48
NBMEMON <proinsaan890@gmail.com>
2024-05-23 15:20:02 -0700

    45 Question Assignment

---
+++ Question-39/index.js
@@ -0,0 +1,6 @@
+function country_city(country, city) {
+    return "\n".concat(country, " ,").concat(city);
+}
+console.log(country_city("Karachi", "Pakistan"));
+console.log(country_city("Berlin", "Germany"));
+console.log(country_city("Moscow", "Russia"));
---
+++ Question-39/index.ts
@@ -0,0 +1,9 @@
+function country_city(country:string , city : string){
+    return `\n${country} ,${city}`;
+}
+
+console.log(country_city("Karachi","Pakistan"))
+
+console.log(country_city("Berlin","Germany"))
+
+console.log(country_city("Moscow","Russia"))
